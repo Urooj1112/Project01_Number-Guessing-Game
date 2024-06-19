acaa169290c3de96be523f9e159d2ce16715767f
Urooj Saeed <165631956+Urooj1112@users.noreply.github.com>
2024-06-19 00:55:31 -0700

    Add files via upload

---
+++ Number Guessing Game/main.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randamNumber=Math.floor(Math.random());
+const answer= await inquirer. prompt([
+    {
+    name:"userGuessedNumber",
+    type:"number",
+    message:"please guess a number between 1-10"
+},
+]);
+if(answer.userGuessedNumber === randamNumber)
+    {
+       console.log("congtratulation! you guessed right number")
+}
+else{
+    console.log("you guessed wrong number");
+};
